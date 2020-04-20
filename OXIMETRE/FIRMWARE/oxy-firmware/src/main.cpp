05e2b8b45e1e53aeade6736ff540e6dfda659a86
JulesTopart <jules.topart@gmail.com>
2020-04-20 16:33:10 +0200

    init(Firmware) : Initialize the firmware project

---
+++ OXIMETRE/FIRMWARE/oxy-firmware/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+ /*                                                      
+ _____             _____ _                             
+|     |_ _ _ _ ___|   __|_|___ _____ _ _ _ ___ ___ ___ 
+|  |  |_'_| | |___|   __| |  _|     | | | | .'|  _| -_|
+|_____|_,_|_  |   |__|  |_|_| |_|_|_|_____|__,|_| |___|
+          |___|                                        
+
+  desc : Firmware for opensource oxymeter
+  author : JulesTopart
+  organization : LaMachinerie, Etoele, Electrolab
+  license: MIT
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
